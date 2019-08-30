b70d2a94533eaff3d39e53e727e6e03eabb466de
Aniol <aniol92@hotmail.com>
2019-08-31 00:03:16 +0200

    First commit

---
+++ OLCCodeJam/World.h
@@ -0,0 +1,37 @@
+#ifndef __WORLD_H_
+#define __WORLD_H_
+
+#include <vector>
+
+enum class RoomType
+{
+	TOWN,
+	WORLD,
+	DUNGEON
+};
+
+enum class IndividualType
+{
+	PLAYER,
+	ENEMY,
+	NPC
+};
+
+enum class ItemType
+{
+	WEAPON,
+	ARMOR,
+	CONSUMABLE
+};
+
+class World
+{
+public:
+	World();
+	~World();
+
+private:
+
+};
+
+#endif // WORLD_H_
